10ea2ee011b15d76d7f2dc5aa07f41f27e39746b
jini <dpwls147@naver.com>
2014-10-19 15:10:53 +0900

    controller servlet명 수정

--- Project/src/controller/Controller.java
+++ Project/src/controller/Controller.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class controller extends HttpServlet {
+public class Controller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		process(request, response);
 	}
